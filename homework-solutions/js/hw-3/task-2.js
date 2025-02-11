e9be6180cd831b82c69d70f15292ec3ca33c62ae
Vitaliya Tsitova <titovavitaliy@gmail.com>
2025-02-12 00:50:38 +0100

    Refactoring task-2

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -5,14 +5,39 @@ n + nn + nnn, где n не перемножаются, а конкатенир�
 
 */
 //variant1
-let positiveNumber = 2;
-let result;
-result = positiveNumber + Number((String(positiveNumber))+(String(positiveNumber))) + Number((String(positiveNumber))+(String(positiveNumber))+(String(positiveNumber))); 
-console.log(result);
+let positiveNumber1 = 2;
+let result1;
 
+result1 = positiveNumber1 
+    + Number(("" + positiveNumber1 + "" + positiveNumber1)) 
+    + Number(("" + positiveNumber1 + "" + positiveNumber1 + "" + positiveNumber1));
+console.log(result1);
 
 //variant2
-let positiveNumber2 = 4;
+
+let positiveNumber2 = 2;
+let stringNumber = String(positiveNumber2);
 let result2;
-result2 = positiveNumber2 + Number((""+ positiveNumber2 + ""+ positiveNumber2)) + Number((""+ positiveNumber2 + ""+ positiveNumber2 +""+ positiveNumber2))
+
+result2 = positiveNumber2 
++ Number(stringNumber + stringNumber) + Number(stringNumber + stringNumber + stringNumber); 
 console.log(result2);
+
+//variant3
+let positiveNumber3 = 2;
+let result3;
+
+result3 = positiveNumber3 
++ +(("" + positiveNumber3 + "" + positiveNumber3)) 
++ +(("" + positiveNumber3 + "" + positiveNumber3 + "" + positiveNumber3));
+console.log(result3);
+
+//variant4
+let positiveNumber4 = 2;
+let result4;
+
+result4 = positiveNumber4 
++ +("".concat(positiveNumber4, positiveNumber4))
++ +("".concat(positiveNumber4, positiveNumber4, positiveNumber4));;
+console.log(result4);
+console.log(typeof(result4));
