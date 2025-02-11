0c6880887dd88b290b740dd169c5a7f0626cdb06
Vitaliya Tsitova <titovavitaliy@gmail.com>
2025-02-11 23:07:53 +0100

    Completed task-2

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -4,3 +4,15 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+//variant1
+let positiveNumber = 2;
+let result;
+result = positiveNumber + Number((String(positiveNumber))+(String(positiveNumber))) + Number((String(positiveNumber))+(String(positiveNumber))+(String(positiveNumber))); 
+console.log(result);
+
+
+//variant2
+let positiveNumber2 = 4;
+let result2;
+result2 = positiveNumber2 + Number((""+ positiveNumber2 + ""+ positiveNumber2)) + Number((""+ positiveNumber2 + ""+ positiveNumber2 +""+ positiveNumber2))
+console.log(result2);
