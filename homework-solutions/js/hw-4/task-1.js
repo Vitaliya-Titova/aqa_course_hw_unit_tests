aaa126fad5e41d37b3e84c29e18e20765b57fc82
Vitaliya Tsitova <titovavitaliy@gmail.com>
2025-02-13 21:32:40 +0100

    Completed task-1

--- homework-solutions/js/hw-4/task-1.js
+++ homework-solutions/js/hw-4/task-1.js
@@ -1,5 +1,24 @@
 /*
 
  - Создайте переменную salary со значением 1000
-  - Создайте переменную grade, которая должна получить значение "middle" если salary больше или равна 1000, и значение "junior" - если меньше
+  - Создайте переменную grade, которая должна получить значение "middle" если salary больше или равна 1000,
+  и значение "junior" - если меньше
+
   */
+
+//var 1
+  let salary = 1000;
+  let grade;
+  
+  if (salary >= 1000){
+        grade = 'middle'
+  } else { 
+     grade = 'junior'
+    };
+    console.log (grade);
+
+//var2 - тернарное ветвление. В лекции это с 1:06
+//условие ? выражение_если_true : выражение_если_false
+let grade2 = salary >= 1000 ? 'middle': 'junior'
+console.log (grade2);
+
