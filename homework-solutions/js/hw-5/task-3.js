32ad53cfcf1675da30142f021a3ad974be1e75d0
Vitaliya Tsitova <titovavitaliy@gmail.com>
2025-02-20 12:38:17 +0100

    Refactoring task 3

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -3,16 +3,20 @@
  * Переменная `word` уже создана и содержит строку со словом.
  * Ожидаемый результат для `hello`: "hello contains 2 vowels and 3 consonants".
  */
-const word = 'hello';
-const letterVowels = 'aeiouy';
+const word = 'He-llo';
+const letterVowels = 'aeiouyAEIOUY';
+const letterConsonants = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
 let vowels=0;
 let consonants=0;
+let noLetter ='0'
 let vowelsAndConsonantsResult = '';
 
 for (let i = 0; i < word.length; i++){
     if (letterVowels.includes(word[i])) {
         vowels = ++vowels
-    } else consonants = ++consonants;  
+    } else if (letterConsonants.includes(word[i]))
+     {consonants = ++consonants;}
+     else (noLetter = ++noLetter)
 };
 
 vowelsAndConsonantsResult = `${word} contains ${vowels} vowels and ${consonants} consonants`;
