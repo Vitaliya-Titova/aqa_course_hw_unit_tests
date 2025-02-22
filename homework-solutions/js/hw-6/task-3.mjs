d0f51d9f1985c7e85478c775ed4500c65afa4cc2
Vitaliya Tsitova <titovavitaliy@gmail.com>
2025-02-22 16:28:30 +0100

    Completed task 3

--- homework-solutions/js/hw-6/task-3.mjs
+++ homework-solutions/js/hw-6/task-3.mjs
@@ -6,6 +6,16 @@
 
   Присвойте результат в переменную "unique"
 */
-let unique;
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
+let unique = [];
+
+for (let element = 0; element < arr.length; element++) {
+  if (unique.includes(arr[element])) {
+    continue;
+  } else {
+    unique.push(arr[element]);
+  }
+}
+console.log(unique);
 
 export { unique };
