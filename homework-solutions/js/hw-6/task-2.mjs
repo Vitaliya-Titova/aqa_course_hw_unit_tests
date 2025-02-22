e5364e28833a03d391492502ed980ad5d48b42fa
Vitaliya Tsitova <titovavitaliy@gmail.com>
2025-02-22 15:29:55 +0100

    Completed task 2

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -8,12 +8,54 @@
   Воспользуйтесь наборами пицц, что приведены ниже.
 
   Пиццы:
-  const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
-  const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
-  const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
-let resultUnique;
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+
+let resultUnique = [];
 let resultNull;
 
+for (let i = 0; i < competitorPizzas.length; i++) {
+  if (myPizzasT1[i] && !competitorPizzas[i].toLowerCase().includes(myPizzasT1[i].toLowerCase())) {
+    resultUnique.push(myPizzasT1[i].toLowerCase());
+  }
+  if (!resultUnique.length) {
+    resultNull = null;
+  }
+}
+for (let element of competitorPizzas) {
+  if (myPizzasT2[element] && !competitorPizzas[element].toLowerCase().includes(myPizzasT2[element].toLowerCase())) {
+    resultUnique.push(myPizzasT2[element].toLowerCase());
+  }
+  if (!resultUnique.length) {
+    resultNull = null;
+  }
+}
+console.log(resultNull);
+console.log(resultUnique);
+
 export { resultNull, resultUnique };
+
+//вар2
+// for (let i = 0; i < competitorPizzas.length; i++) {
+//   if (myPizzasT1[i] && competitorPizzas[i].toLowerCase() !== myPizzasT1[i].toLowerCase()) {
+//     resultUnique.push(myPizzasT1[i].toLowerCase());
+//   }
+//   if (!resultUnique.length) {
+//     resultNull = null;
+//   }
+// }
+// for (let i = 0; i < competitorPizzas.length; i++) {
+//   if (myPizzasT2[i] && competitorPizzas[i].toLowerCase() !== myPizzasT2[i].toLowerCase()) {
+//     resultUnique.push(myPizzasT2[i].toLowerCase());
+//   }
+//   if (!resultUnique.length) {
+//     resultNull = null;
+//   }
+// }
+
+// console.log(resultNull);
+// console.log(resultUnique);
+// export { resultNull, resultUnique };
