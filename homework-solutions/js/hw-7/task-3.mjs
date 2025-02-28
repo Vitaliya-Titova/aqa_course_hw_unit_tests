82983c5b4c15096b1e53993649e93b92b0cc5565
Vitaliya Tsitova <titovavitaliy@gmail.com>
2025-02-28 14:37:39 +0100

    Completed task 3

--- homework-solutions/js/hw-7/task-3.mjs
+++ homework-solutions/js/hw-7/task-3.mjs
@@ -5,11 +5,11 @@
   И так пока, сумма не станет меньше либо равной 9. 
   После окончания сложений возвращает полученное число.
   Например при подаче числа 19 (1+9=10>9, потому 1+0=1) выводится 1
-
 */
 
 function digitalRoot(number) {
-  // Ваш код
+  return number <= 9 ? number : digitalRoot((number % 10) + (number - (number % 10)) / 10);
 }
+console.log(digitalRoot(1233448));
 
 export { digitalRoot };
