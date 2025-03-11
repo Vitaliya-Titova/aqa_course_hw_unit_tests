71a45ba8d594220db8af9f77edd998429d372c8b
Vitaliya Tsitova <titovavitaliy@gmail.com>
2025-03-11 23:07:43 +0100

    Completed task 1

--- homework-solutions/js/hw-10/task-1.mjs
+++ homework-solutions/js/hw-10/task-1.mjs
@@ -5,7 +5,15 @@
     Значения в строке должны ссылаться на контекст ЭТОГО ОБЪЕКТА, без подмен.
 */
 
-let qa;
+let qa = {
+  name: 'Vita',
+  age: 31,
+  salary: 2000,
+  getInfo(greetingsWord) {
+    return `${greetingsWord}, my name is ${this.name}, I'm ${this.age} and my salary is ${this.salary}`;
+  },
+};
+console.log(qa.getInfo('Witam'));
 
 /*
  2. Changing the context
@@ -14,18 +22,23 @@ let qa;
   - Вызовите метод getInfo объекта qa c контекстом вызова объекта anotherQa с помощью метода call()
   - Вызовите метод getInfo объекта qa c контекстом вызова объекта anotherQa с помощью метода apply()
  */
-
-let anotherQa;
+let anotherQa = {
+  name: 'Pasha',
+  age: 5,
+  salary: 5000,
+};
 
 // Используйте bind с greetingWord "Hello"
-let bindResult;
-
+let bindResult = qa.getInfo.bind(anotherQa)('Hello');
+console.log(bindResult);
 // Используйте call с greetingWord "Hi"
-let callResult;
+let callResult = qa.getInfo.call(anotherQa, 'Hi');
+console.log(callResult);
 
 // Используйте apply с greetingWord "Hey"
-let applyResult;
 
+let applyResult = qa.getInfo.call(anotherQa, ['Hey']);
+console.log(applyResult);
 /*
  3. Closures
   - Создайте функцию createCounter(),
